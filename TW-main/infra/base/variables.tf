9c9f2ebab9c273c20bb5f8a0c07e1a72444d36bf
Sundar Jayabalakrishnan <100151654+SundarJay@users.noreply.github.com>
2025-12-07 19:20:16 +0530

    Add files via upload

---
+++ TW-main/infra/base/variables.tf
@@ -0,0 +1,7 @@
+variable "prefix" {
+  default = "newssj3"
+}
+
+variable "location" {
+  default = "North Europe" 
+}
---
+++ TW-main/infra/news/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+  default = "newssj3"
+}
+
+variable "location" {
+  default = "North Europe"
+}
+
+variable "acr_url_default" {
+  default = ".azurecr.io"
+}
